507105c09dc47165b2394711f6cb180252603968
Kaspar Manz <kaspar@xeophin.net>
2018-06-26 11:07:52 -0400

    chore(Classwork 07): add leaflet templates

---
+++ classwork-07/map_points_template/geo-data.js
@@ -0,0 +1,71 @@
+infoData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties":{ "name": "here", "group_name": "favs", "group_id": 1, "headline": "home", "article": "<p>some text<\/p>", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96416664123535,
+          40.78950978441437
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "bests", "group_id": 2, "headline": "special place", "article": "<p> this is my favorite place in the park!<\/p>", "color": "#666600", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96631240844727,
+          40.78827508312983
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "favs", "group_id": 1, "headline": "home", "article": "<p>some text<\/p>", "color": "#660000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96734237670898,
+          40.78580561168767
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "favs", "group_id": 1, "headline": "Center of the reservoir", "article":  "<p> don't swim here<\/p>", "color": "#000000", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96184921264648,
+          40.78645548149673
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "favs", "group_id": 1, "headline": "home", "article": "<p>some text<\/p>", "color": "#666600", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96159172058104,
+          40.78216622323578
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "name": "here", "group_name": "favs", "group_id": 1, "headline": "lots of jogging", "article": "<h1> an article on jogging</h1><p>some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text <\/p>", "color": "#660000", "radius": "12" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.95764350891112,
+          40.78808012819681
+        ]
+      }
+    }
+  ]
+}
