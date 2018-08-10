96d2d0995db197a81746749380e9fce5b1b6a6b8
Kaspar Manz <kaspar@xeophin.net>
2018-08-10 17:05:11 -0400

    feat: download genre data from spotify
    And save this information to the database (directly).

--- project/common.py
+++ project/common.py
@@ -1,6 +1,6 @@
 import re
 import uuid
-from typing import List
+from typing import List, Optional
 
 from slugify import slugify
 
@@ -25,17 +25,27 @@ def normalise_artist_names(name: str) -> str:
   return slugify(name.strip())
 
 
+def create_uuid(identificator: str, prefix: Optional[str] = None) -> uuid:
+  return uuid.uuid3(
+    uuid.NAMESPACE_URL,
+    prefix + identificator)
+
+
 def get_uuid_for_artist(artist_name: str) -> uuid:
   """
   Generates the UUID for an artist name.
   :param artist_name: The (normalised) artist's name
   :return: The UUID for this artist
   
+  >>> get_uuid_for_artist('Céline Dion')
+  UUID('5ddcab91-0721-3997-bae0-7411c2e286ec')
+  
+  >>> get_uuid_for_artist('Celine Dion')
+  UUID('5ddcab91-0721-3997-bae0-7411c2e286ec')
+  
   """
   PREFIX = 'internal://artist/'
-  return uuid.uuid3(
-    uuid.NAMESPACE_URL,
-    PREFIX + normalise_artist_names(artist_name))
+  return create_uuid(normalise_artist_names(artist_name), PREFIX)
 
 
 def split_creators(creators: str) -> List[str]:
@@ -52,3 +62,7 @@ def split_creators(creators: str) -> List[str]:
   pattern = r'\s*[,/&+()]+\s*|\Wand\W'
   splitted = re.split(pattern, creators)
   return splitted
+
+
+def normalise_tag_name(tag):
+  return slugify(tag, regex_pattern=re.compile(r'[^a-z]+'))
--- project/spotify-data.py
+++ project/spotify-data.py
@@ -1,24 +1,10 @@
 import pg8000
 
+from project.apikeys import SPOTIFY_CREDENTIALS
+from project.common import normalise_tag_name, create_uuid
+
 conn = pg8000.connect('km', database='esc')
 cursor = conn.cursor()
-
-# %%
-myquery = '''
-SELECT a.name, count(f.song)
-FROM composers
-       JOIN artists a ON composers.artist_id = a.id
-       JOIN finalists f ON composers.song_id = f.id
-WHERE f.ranking < 4
-GROUP BY a.name
-ORDER BY count DESC;
-'''
-cursor.execute(myquery)
-myresult = cursor.fetchall()
-for name, count in myresult[:10]:
-  print('Komponist {} hat {} Beiträge'.format(name, count))
-
-# Great, so connecting directly to the PostgreSQL data base works.
 
 #### ARTIST GENRES ######
 # %% load the artist list
@@ -30,33 +16,61 @@ FROM performers p
 cursor.execute(performer_query)
 performers = cursor.fetchall()
 
-# %%
-from project.spotify import get_authenticated_session
+# %% Setup spotipy with the correct credentials and a token
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-spotify = get_authenticated_session()
+client_credentials_manager = SpotifyClientCredentials(**SPOTIFY_CREDENTIALS)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # %%
 # Loop through the list, search for artist on spotify, get spotify ID
-# https://developer.spotify.com/documentation/web-api/reference/search/search/
-SPOTIFY_ENDPOINT_SEARCH = 'https://api.spotify.com/v1/search'
 for performer, uuid in performers[:10]:
   params = {
     'q': performer,
     'type': 'artist'
   }
 
-  r = spotify.get(SPOTIFY_ENDPOINT_SEARCH, params=params)
-  results = r.json()
+  results = spotify.search(**params)
   if results['artists']['total'] > 0:
     first_hit = results['artists']['items'][0]
     genres = first_hit['genres']
     name = first_hit['name']
-  
-    print('Looking for {} yielded {} with genres {}'.format(
+    id = first_hit['id']
+
+    print('Looking for {} yielded {}, {} with genres {}'.format(
       performer,
-      name, ', '.join(genres)))
+      name, id, ', '.join(genres)))
+
+    # save artist Spotify ID to database, just to be sure
+    cursor.execute("UPDATE artists SET spotify_id = %s WHERE id = %s",
+                   (id, uuid))
+
+    # normalise genres, write to db
+    for genre in genres:
+      normalised_genre = normalise_tag_name(genre)
+      genre_uuid = create_uuid(normalised_genre, 'internal://genre/')
+      
+      # Add the genre to the database (unless we already found it at some 
+      # point, then just do nothing)
+      add_to_genre_query = """
+      INSERT INTO genres (id, normalised_name, name) VALUES (%s, %s, %s) ON 
+      CONFLICT DO NOTHING
+      """
+      cursor.execute(add_to_genre_query, (genre_uuid, normalised_genre, genre))
+      
+      # Add the connection, so we know which artist has what genre.
+      add_to_conn_table_query = """
+      INSERT INTO genres_conn (artist_id, genre_id) VALUES (%s, %s) ON CONFLICT DO NOTHING 
+      """
+      cursor.execute(add_to_conn_table_query, (uuid, genre_uuid))
+
+
   else:
     print('Found nothing for {}.'.format(performer))
+
+# Commit all the database changes back to the database
+conn.commit()
 
 # Save the list back to CSV
 
@@ -68,3 +82,7 @@ for performer, uuid in performers[:10]:
 # https://developer.spotify.com/documentation/web-api/reference/search/search/
 
 # https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-features/
+
+
+# Close the database connection
+conn.close()
